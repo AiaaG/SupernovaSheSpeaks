70a4e4912b6597658e383b4c35894e06b6574d38
Aia <115946987+AiaaG@users.noreply.github.com>
2025-05-05 14:47:03 +0700

    Create index.dart

---
+++ lib/index.dart
@@ -0,0 +1,7 @@
+library mobile_app;
+
+export 'screens/diary.dart';
+export 'screens/homepage.dart';
+export 'screens/onboarding.dart';
+export 'screens/stories.dart';
+export 'screens/support.dart';
